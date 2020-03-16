e35b21a84a72da27dbaacf6e9e1c543403a6846b
AndreaLoNivuro <a.lonivuro@live.it>
2020-03-16 17:12:24 +0100

    Modifica primo component

--- src/app/primo/primo.component.ts
+++ src/app/primo/primo.component.ts
@@ -18,6 +18,15 @@ export class PrimoComponent implements OnInit {
   @Input()
   titoloInput: string;
 
+  lista = [1,2,5,6];
+
+  myDate: Date = new Date();
+
+  oggetto: {chiave: string};
+  oggetto1: {chiave: string} = {
+    chiave: 'valore'
+  }
+
   constructor() { }
 
   ngOnInit(): void {

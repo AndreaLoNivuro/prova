a8b9c0bd843009730dea32ca8be4de0b3254b8f8
AndreaLoNivuro <a.lonivuro@live.it>
2020-03-29 20:14:57 +0200

    prima del routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { PrimoComponent } from './primo/primo.component';
 import { SecondoComponent } from './secondo/secondo.component';
 import { CiclovitaComponent } from './ciclovita/ciclovita.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MenuComponent } from './menu/menu.component';
+
+const appRoutes: Routes = [
+  { path: 'primo', component: PrimoComponent },
+  { path: 'secondo',      component: SecondoComponent },
+  { path: 'ciclovita',      component: CiclovitaComponent },
+  { path: '', redirectTo: '/primo', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent },
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimoComponent,
     SecondoComponent,
-    CiclovitaComponent
+    CiclovitaComponent,
+    PageNotFoundComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]

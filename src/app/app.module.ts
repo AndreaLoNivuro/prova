2eb55c6d55f60752586a761f9893ece99dd8bf09
AndreaLoNivuro <a.lonivuro@live.it>
2020-03-29 20:31:47 +0200

    modifiche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { PrimoComponent } from './primo/primo.component';
 import { SecondoComponent } from './secondo/secondo.component';
 import { CiclovitaComponent } from './ciclovita/ciclovita.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { MenuComponent } from './menu/menu.component';
-
-const appRoutes: Routes = [
-  { path: 'primo', component: PrimoComponent },
-  { path: 'secondo',      component: SecondoComponent },
-  { path: 'ciclovita',      component: CiclovitaComponent },
-  { path: '', redirectTo: '/primo', pathMatch: 'full'},
-  { path: '**', component: PageNotFoundComponent },
-];
-
 
 @NgModule({
   declarations: [
@@ -26,15 +14,11 @@ const appRoutes: Routes = [
     PrimoComponent,
     SecondoComponent,
     CiclovitaComponent,
-    PageNotFoundComponent,
-    MenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(
-      appRoutes,
-    ),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]

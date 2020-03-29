27703a88ae18d958e53afb5b172c31b81dea5406
AndreaLoNivuro <a.lonivuro@live.it>
2020-03-29 23:22:36 +0200

    my route guard service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,11 @@ import { PrimoComponent } from './primo/primo.component';
 import { SecondoComponent } from './secondo/secondo.component';
 import { CiclovitaComponent } from './ciclovita/ciclovita.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MyRouteGuardService } from './my-route-guard.service';
 
 const appRoutes: Routes = [
     { path: 'primo', component: PrimoComponent },
-    { path: 'secondo', component: SecondoComponent },
+    { path: 'secondo', component: SecondoComponent, canActivate: [MyRouteGuardService] },
     { path: 'ciclovita', component: CiclovitaComponent },
     { path: '', redirectTo: '/primo', pathMatch: 'full' },
     { path: '**', component: PageNotFoundComponent },
---
+++ src/app/my-route-guard.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MyRouteGuardService implements CanActivate {
+
+  constructor(private router: Router) { }
+  
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    this.router.navigateByUrl('/primo');
+    return true;
+  }
+}
